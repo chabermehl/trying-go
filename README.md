a5179c9762c381351d15e33e5e3e6f3dee48d78e
chabermehl <clp.habermehl@gmail.com>
2024-02-10 21:43:45 -0700

    Starting Go tutorial...

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trying Go
+
+Following the official Go tutorial.
+
+I am [here](https://go.dev/doc/tutorial/greetings-multiple-people).
---
+++ hello/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	message, err := greetings.Hello("Charles")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
